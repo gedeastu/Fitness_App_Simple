0cfc5d94e17d37cf52949e256bf472c68aa113e6
gedeastu <gede.astu@gmail.com>
2023-12-17 21:07:07 +0700

    up

---
+++ fitness/lib/pages/breakfast.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Breakfast extends StatefulWidget {
+  const Breakfast({super.key});
+
+  @override
+  State<Breakfast> createState() => _BreakfastState();
+}
+
+class _BreakfastState extends State<Breakfast> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ,
+    );
+  }
+}
